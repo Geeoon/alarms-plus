102fc557a67055e4a6a60ab35df431fc92132312
Geeoon Chung <geeoon13@gmail.com>
2023-07-28 16:49:02 -0700

    Synced visuals and state

--- src/alarms-plus/components/NewAlarmScreen.js
+++ src/alarms-plus/components/NewAlarmScreen.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { Alert, StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import CalendarPicker from 'react-native-calendar-picker';
 import { useDispatch, useSelector } from 'react-redux';
@@ -12,6 +12,8 @@ export default function NewAlarmScreen({navigation}) {
   
   const [name, setName] = useState('');
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
   const [repeats, setRepeats] = useState(5);
   const [repeatInterval, setRepeatInterval] = useState(5);
 
@@ -35,24 +37,82 @@ export default function NewAlarmScreen({navigation}) {
       }
   }, [name, date, repeats, repeatInterval, alarmArray]);
 
+  useEffect(() => {
+    // set hours, minutes to current date
+    setHours(parseInt((date.getHours() >= 0) ? ((date.getHours() % 12) === 0 ? 12 : date.getHours() % 12) : 12));
+    setMinutes(date.getMinutes());
+  }, []);
+
   return (
     <View style={styles.container}>
         <View>
             <TextInput style={styles.textInput} placeholder='Alarm Name' placeholderTextColor='#999999' onChangeText={text => setName(text)}/>
             <View style={styles.inputContainer}>
                 <Text style={styles.text}>Number of Repeats</Text>
-                <TextInput onChangeText={text => setRepeats((text != '') ? parseInt(text) : 0)} style={styles.numberInput} inputMode='numeric' placeholderTextColor='#999999' value={'' + repeats}/>
+                <TextInput
+                  onChangeText={text => setRepeats(text)}
+                  onEndEditing={(e) => setRepeats((e.nativeEvent.text != '') ? parseInt(e.nativeEvent.text) : 0)}
+                  style={styles.numberInput}
+                  inputMode='numeric'
+                  placeholderTextColor='#999999'
+                  value={'' + repeats}
+                />
             </View>
             <View style={styles.inputContainer}>
                 <Text style={styles.text}>Minutes Between Repeats</Text>
-                <TextInput onChangeText={text => setRepeatInterval((text != '') ? parseInt(text) : 1)} style={styles.numberInput} inputMode='numeric' placeholderTextColor='#999999' value={'' + repeatInterval}/>
+                <TextInput
+                  onChangeText={text => setRepeatInterval(text)}
+                  onEndEditing={(e) => setRepeatInterval((e.nativeEvent.text != '') ? parseInt(e.nativeEvent.text) : 1)}
+                  style={styles.numberInput}
+                  inputMode='numeric'
+                  placeholderTextColor='#999999'
+                  value={'' + repeatInterval}
+                />
             </View>
             <View style={styles.inputContainer}>
               <Text style={styles.text}>Time: </Text>
-              <TextInput onChangeText={(text) => {}} style={styles.numberInput} inputMode='numeric' value={date.toLocaleTimeString().split(':')[0]}/>
+              <TextInput
+                onChangeText={text => setHours(text)}
+                onEndEditing={(text) => {
+                  if (text != '') {
+                    if (hours < 1) {
+                      setDate(new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, date.getMinutes(), date.getMilliseconds()));
+                    } else if (hours > 23) {
+                      setDate(new Date(date.getFullYear(), date.getMonth(), date.getDate(), hours % 24, date.getMinutes(), date.getMilliseconds()));
+                    } else {
+                      setDate(new Date(date.getFullYear(), date.getMonth(), date.getDate(), parseInt(hours), date.getMinutes(), date.getMilliseconds()));
+                    }
+                  } else {
+                    setDate(new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, date.getMinutes(), date.getMilliseconds()));
+                  }
+                  setHours(parseInt((hours >= 0) ? ((hours % 12) === 0 ? 12 : hours % 12) : 12));
+                }}
+                style={styles.numberInput}
+                inputMode='numeric'
+                value={'' + hours}
+              />
               <Text style={styles.text}>:</Text>
-              <TextInput onChangeText={(text) => {}} style={styles.numberInput} inputMode='numeric' value={date.toLocaleTimeString().split(':')[1]}/>
-              <Button title={date.toLocaleTimeString().split(' ')[1]} onPress={() => {date.getHours() >= 12 ?
+              <TextInput
+                onChangeText={text => setMinutes(text)}
+                onEndEditing={(text) => {
+                  if (text != '') {
+                    if (minutes < 0) {
+                      setDate(new Date(dathourse.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), 0, date.getMilliseconds()));
+                    } else if (hours >= 60) {
+                      setDate(new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), minutes % 60, date.getMilliseconds()));
+                    } else {
+                      setDate(new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), parseInt(minutes), date.getMilliseconds()));
+                    }
+                  } else {
+                    setDate(new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), 0, date.getMilliseconds()));
+                  }
+                  setMinutes(parseInt((minutes >= 0) ? ((minutes < 60) ? minutes : 59) : 0));
+                }}
+                style={styles.numberInput}
+                inputMode='numeric'
+                value={'' + minutes}
+              />
+              <Button title={date.getHours() > 11 ? "PM" : "AM"} onPress={() => {date.getHours() >= 12 ?
                 setDate(new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours() - 12, date.getMinutes(), date.getMilliseconds())) : 
                 setDate(new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours() + 12, date.getMinutes(), date.getMilliseconds()))
               }}/>
