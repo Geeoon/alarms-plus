f25722686996c8840c68254c51d66db3118a22c0
Geeoon Chung <geeoon13@gmail.com>
2023-08-07 14:44:33 -0700

    Using expo calendar

--- src/alarms-plus/components/Alarm.js
+++ src/alarms-plus/components/Alarm.js
@@ -1,11 +1,11 @@
 import React, { useCallback } from 'react';
 import { Button, StyleSheet, Text, View, Alert } from 'react-native';
-import { useDispatch } from 'react-redux';
-import * as Notifications from 'expo-notifications';
-
+import { useDispatch, useSelector } from 'react-redux';
+import * as Calendar from 'expo-calendar';
 import { removeAlarm } from '../Redux/Alarms/alarmsSlice';
 
 export default function Alarm(props) {
+    const calId = useSelector((state) => state.alarms.calendarId);
     const dispatch = useDispatch();
     const onDelete = useCallback(() => {
         Alert.alert(`Delete "${props.name}" Alarm`, 'Are you sure you want to delete this alarm?', [
@@ -16,13 +16,18 @@ export default function Alarm(props) {
             {
                 text: 'YES',
                 onPress: async () => {
-                    // reset all scheduled notifications
-                    await Notifications.cancelAllScheduledNotificationsAsync();
+                    // delete all scheduled events
+                    let events = await Calendar.getEventsAsync([calId], new Date(0), new Date(4121373985000));
+                    for (let i = 0; i < events.length; i++) {
+                        await Calendar.deleteEventAsync(events[i].id);
+                    }
                     dispatch(removeAlarm({ name: props.name }));
                  },
             },
         ]);
-    }, [props.name]);
+    }, [props.name, calId]);
+
+
 
     return (
         <View style={styles.container}>
